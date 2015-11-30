6abeedb4b940f5131e8b8a35f9eb9bd079586c7b
unknown <Kanchan.verma@NDI-LAP-327.3pillar.corp>
2015-11-30 17:42:14 +0530

    committing updated file to test UntrustedInputInFind rule

--- JacksMongo_UntrustedInputInFind.js
+++ JacksMongo_UntrustedInputInFind.js
@@ -8,6 +8,6 @@
 //db.jacksCollection.find({value:FindNumber(x)});
 
 app.post('/', function (req, res) { db.users.find({username: req.body.username, password: req.body.password}, function (err, users) { 
-  // TODO: handle the rest 
+  var a = req.body.username
   }); 
 });
