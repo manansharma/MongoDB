e4332966763c185aae749d5b3195967c9f0cb15d
unknown <Kanchan.verma@NDI-LAP-327.3pillar.corp>
2015-12-02 13:37:42 +0530

    committing updated file to test UntrustedInputInInsert rule

--- JacksMongo_UntrustedInputInInsert.js
+++ JacksMongo_UntrustedInputInInsert.js
@@ -1,8 +1,24 @@
-function addNumbers( x , y ) {
-    return x + y;
-}
+//function addNumbers( x , y ) {
+  //  return x + y;
+//}
+
+//use jacksMongoDbTest;
+//db.createCollection("jacksCollection"); 
+
+//db.jacksCollection.insert({id:"addNumbers", value:addNumbers(x, y)});
 
 use jacksMongoDbTest;
 db.createCollection("jacksCollection"); 
 
-db.jacksCollection.insert({id:"addNumbers", value:addNumbers(x, y)});
+var mydocuments;
+
+function get_data(name, category){
+	mydocuments = [{
+		name_atr: name,
+		cat_atr: category
+	}];
+}
+
+db.jacksCollection.insert(mydocuments);
+
+
