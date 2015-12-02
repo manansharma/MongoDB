d49085e37c93ea1709c1512b19d9379dde6a907a
unknown <Kanchan.verma@NDI-LAP-327.3pillar.corp>
2015-12-02 17:08:07 +0530

    committing updated file to test UntrustedInputInInsert rule

--- Test/JacksMongo_UntrustedInputInInsert.js
+++ Test/JacksMongo_UntrustedInputInInsert.js
@@ -10,15 +10,17 @@
 use jacksMongoDbTest;
 db.createCollection("jacksCollection"); 
 
-var mydocuments;
+function (req, res) { db.users.insert(username: req.body.username, password: req.body.password)};
 
-function get_data(name, category){
-	mydocuments = [{
-		name_atr: name,
-		cat_atr: category
-	}];
-}
+//var mydocuments;
 
-db.jacksCollection.insert(mydocuments);
+//function get_data(name, category){
+	//mydocuments = [{
+		//name_atr: name,
+		//cat_atr: category
+	//}];
+//}
+
+//db.jacksCollection.insert(mydocuments);
 
 
